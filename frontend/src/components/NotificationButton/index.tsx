5052b94f8d64df47c87e41e48cda480dfdaa6213
aa <christian.azonyetin@estudante.ufla.br>
2022-07-13 18:07:34 -0300

    First component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,13 @@
+import icon from '../../assets/img/notification-icone.svg'
+import './styles.css'
+function NotificationButton() {
+    return(
+<       div className="dsmeta-red-btn">
+            <img src={icon} alt="notificar" />
+        </div>    
+    )
+    
+}
+  
+export default NotificationButton 
+  
